9aaf030542057eb699edd8f22f0e1e7141262480
Mykokola <119622278+Mykokola@users.noreply.github.com>
2023-10-03 15:35:23 -0500

    add hash password

--- backend/src/api/usersAuth.ts
+++ backend/src/api/usersAuth.ts
@@ -2,9 +2,10 @@ import { Request, Response, NextFunction } from 'express';
 const {createError} = require('../untils/createError')
 const userService  = require('../services/usersBasicService')
 const ERROR_TYPES = require('../constants/errorTypes')
+const bcrypt = require('bcrypt')
 const registerUser = async (req:Request,res:Response,next:NextFunction) => {
     try{
-    const {email} = req.body
+    const {email,password} = req.body
     const dubliceteUser:{} = (await userService.getUserOption({email}))[0]
     if(dubliceteUser){
         const error = await createError(ERROR_TYPES.CONFLICT,{
@@ -12,7 +13,13 @@ const registerUser = async (req:Request,res:Response,next:NextFunction) => {
         })
         throw error
     }
-    let user= await userService.regUser(req.body)
+    const passwordHash = await bcrypt.hash(password,10)
+
+    const newUser = {...req.body,
+        password:passwordHash
+    }
+    console.log(newUser)
+    let user= await userService.regUser(newUser)
       res.status(200).json({
         data: user,
       });
@@ -20,6 +27,15 @@ const registerUser = async (req:Request,res:Response,next:NextFunction) => {
         next(e)
     }
   }
+const loginUser = async (req:Request,res:Response,next:NextFunction) => {
+    try{
+
+
+
+    }catch(e){
+        next(e)
+    }
+}
 
 
   export = registerUser
